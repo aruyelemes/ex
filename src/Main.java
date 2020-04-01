69cfe515543d12fc5ed78ce7974ba427e5898252
Aru <aru.yelemes23@gmail.com>
2020-04-01 17:23:59 +0600

    Commit

---
+++ src/Main.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b =scan.nextInt();
+        int c= scan.nextInt();
+
+        int count = 0;
+        if(a>0){
+           count++;
+        }if(b>0){
+            count++;
+        }if(c>0){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
